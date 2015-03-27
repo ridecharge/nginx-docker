d5e7d28b523b5f6cca6dce68b64519ef85ef511d
Stephen Garlick <sgarlick@taximagic.com>
2015-03-27 10:06:09 -0400

    auto reload config

--- nginx-wrapper.sh
+++ nginx-wrapper.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sed -i "s:SERVICE_NAME:$SERVICE_NAME:g" /etc/confd/conf.d/nginx.toml
-/usr/bin/confd -onetime -backend consul -node consul:8500
+
+/usr/bin/confd -backend consul -node consul:8500 \
+	-debug -verbose -config-file /etc/confd/conf.d/nginx.toml -watch &
+echo "[nginx] starting..."
 exec /usr/sbin/nginx
