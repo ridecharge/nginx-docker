4d5aa8d4d0728847cbdc39c0a2a5b9a19b5f0c40
Stephen Garlick <sgarlick@taximagic.com>
2015-04-04 22:49:33 -0400

    confd -onetime before starting in bg

--- nginx-wrapper.sh
+++ nginx-wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sed -i "s:SERVICE_NAME:$SERVICE_NAME:g" /etc/confd/conf.d/nginx.toml
 
+/usr/bin/confd -onetime -backend consul -node consul:8500 \
+	-debug -verbose -config-file /etc/confd/conf.d/nginx.toml
+
 /usr/bin/confd -backend consul -node consul:8500 \
 	-debug -verbose -config-file /etc/confd/conf.d/nginx.toml -watch &
 echo "[nginx] starting..."
