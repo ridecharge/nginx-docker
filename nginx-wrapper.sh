6b51e4939d1da92c79bf095b937d0e4956cf2dcf
Stephen Garlick <sgarlick@taximagic.com>
2015-04-07 13:56:03 -0400

    warpper updated

--- nginx-wrapper.sh
+++ nginx-wrapper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sed -i "s:SERVICE_NAME:$SERVICE_NAME:g" /etc/confd/conf.d/nginx.toml
 
+# This will error when reloading nginx since it's not started
+# but this is not a problem.
 /usr/bin/confd -onetime -backend consul -node consul:8500 \
 	-debug -verbose -config-file /etc/confd/conf.d/nginx.toml
 
 /usr/bin/confd -backend consul -node consul:8500 \
 	-debug -verbose -config-file /etc/confd/conf.d/nginx.toml -watch &
-echo "[nginx] starting..."
+
 exec /usr/sbin/nginx
